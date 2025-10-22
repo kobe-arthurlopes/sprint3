bd21e36ff9556bb2a6279fe851b84bd8c05d0df3
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-22 09:32:14 -0300

    feat: create contentful model (blog post)

---
+++ sprint3_app/lib/models/blog_post.dart
@@ -0,0 +1,34 @@
+import 'package:contentful/contentful.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'blog_post.g.dart';
+
+@JsonSerializable()
+class BlogPost extends Entry<BlogPostFields> {
+  BlogPost({
+    required SystemFields sys,
+    required BlogPostFields fields,
+  }) : super(sys: sys, fields: fields);
+
+  static BlogPost fromJson(Map<String, dynamic> json) =>
+      _$BlogPostFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BlogPostToJson(this);
+}
+
+@JsonSerializable()
+class BlogPostFields extends Equatable {
+  final String? title;
+  final String? body;
+
+  const BlogPostFields({this.title, this.body}) : super();
+
+  static BlogPostFields fromJson(Map<String, dynamic> json) =>
+      _$BlogPostFieldsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BlogPostFieldsToJson(this);
+
+  @override
+  List<Object?> get props => [title, body];
+}
