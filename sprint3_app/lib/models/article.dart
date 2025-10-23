a39997844539ffd6a33101f7f5464402c886bc9d
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-23 15:34:50 -0300

    feat: create article model

---
+++ sprint3_app/lib/models/article.dart
@@ -0,0 +1,37 @@
+class ArticleResponse {
+  final List<Article> articles;
+
+  ArticleResponse({required this.articles});
+
+  factory ArticleResponse.fromJson(Map<String, dynamic> json) {
+    return ArticleResponse(
+      articles: List.from(
+        json['articles'],
+      ).map((element) => Article.fromJson(element)).toList(),
+    );
+  }
+
+  static ArticleResponse parseArticleResponse(Map<String, dynamic> json) {
+    return ArticleResponse.fromJson(json);
+  }
+}
+
+class Article {
+  final String name;
+  final String description;
+  final String url;
+
+  Article({
+    required this.name,
+    required this.description,
+    required this.url
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      name: json['source']['name'], 
+      description: json['description'], 
+      url: json['url']
+    );
+  }
+}
