326df1aaa0067695975e03d1314025dd607029b6
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-24 10:16:53 -0300

    feat: create news sources list and items

---
+++ sprint3_app/lib/widgets/news_source_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:sprint3_app/models/news_source.dart';
+
+class NewsSourceItem extends StatelessWidget {
+  final NewsSource newsSource;
+
+  const NewsSourceItem({super.key, required this.newsSource});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 10,
+        left: 10,
+        right: 10
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+              newsSource.fields?.logoUrl ?? 'none'
+            ),
+          ),
+      
+          SizedBox(
+            width: 100,
+            height: 40,
+            child: Center(
+              child: Text(
+                newsSource.fields?.name ?? 'none',
+                textAlign: TextAlign.center,
+              )
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ sprint3_app/lib/widgets/news_sources_list.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:sprint3_app/models/news_source.dart';
+import 'package:sprint3_app/widgets/news_source_item.dart';
+
+class NewsSourcesList extends StatefulWidget {
+  final List<NewsSource> newsSources;
+
+  const NewsSourcesList({super.key, required this.newsSources});
+
+  @override
+  State<StatefulWidget> createState() => _NewsSourcesListState();
+}
+
+class _NewsSourcesListState extends State<NewsSourcesList> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 10
+          ),
+          child: Text(
+            'Top News Sources',
+            style: TextStyle(
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+
+        SizedBox(
+          height: 160,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: widget.newsSources.length,
+            itemBuilder: (context, index) {
+              final newsSource = widget.newsSources[index];
+        
+              return NewsSourceItem(newsSource: newsSource);
+            }
+          ),
+        ),
+      ],
+    );
+  }
+}
