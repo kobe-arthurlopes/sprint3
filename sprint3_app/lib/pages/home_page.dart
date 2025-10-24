bf857d5120d9c68a4031b19863ce10fc2a6f606f
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-24 10:16:29 -0300

    feat: change home page to receive new ui

--- sprint3_app/lib/pages/home_page.dart
+++ sprint3_app/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sprint3_app/view_models/home_view_model.dart';
+import 'package:sprint3_app/widgets/news_sources_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -29,49 +30,9 @@ class _HomePageState extends State<HomePage> {
       builder: (_, data, _) {
         return Scaffold(
           appBar: AppBar(
-            title: const Text("News"),
+            title: const Text('News'),
           ),
-          body: ListView.builder(
-            itemCount: data.newsSources.length,
-            itemBuilder: (context, index) {
-              final newsSource = data.newsSources[index];
-
-              return Container(
-                color: Colors.greenAccent,
-                margin: const EdgeInsets.only(
-                  top: 10,
-                  left: 10,
-                  right: 10
-                ),
-                padding: const EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: Image.network(
-                        newsSource.fields?.logoUrl ?? '',
-                        filterQuality: FilterQuality.high,
-                        fit: BoxFit.cover,
-                        height: 100,
-                        width: 100,
-                      ),
-                    ),
-                
-                    Text(
-                      newsSource.fields?.name ?? 'none',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40
-                      ),
-                    )
-                  ],
-                ),
-              );
-            },
-          ),
+          body: NewsSourcesList(newsSources: data.newsSources),
         );
       }
     );
