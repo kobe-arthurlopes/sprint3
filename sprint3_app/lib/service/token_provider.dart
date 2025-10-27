99fbe591303774e1e6ad8d7e55fec6ac712dd85c
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-27 09:10:40 -0300

    fix: change token provider class to get tokens from firebase and not from .env file

--- sprint3_app/lib/service/token_provider.dart
+++ sprint3_app/lib/service/token_provider.dart
@@ -1,20 +1,27 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:sprint3_app/firebase_options.dart';
 
 class TokenProvider {
-  late final String? contentfulAccessToken;
-  late final String? contentfulSpaceId;
-  late final String? apiKey;
+  final String? contentfulAccessToken;
+  final String? contentfulSpaceId;
+  final String? apiKey;
 
   TokenProvider._(this.contentfulAccessToken, this.contentfulSpaceId, this.apiKey);
 
   static Future<TokenProvider> create() async {
-    await dotenv.load(fileName: '.env');
-    final Map<String, String> env = dotenv.env;
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+    );
+
+    final remoteConfig = FirebaseRemoteConfig.instance;
+
+    await remoteConfig.fetchAndActivate();
 
     return TokenProvider._(
-      env['CONTENTFUL_ACCESS_TOKEN'], 
-      env['CONTENTFUL_SPACE_ID'], 
-      env['API_KEY']
+      remoteConfig.getString('CONTENTFUL_ACCESS_TOKEN'),
+      remoteConfig.getString('CONTENTFUL_SPACE_ID'),
+      remoteConfig.getString('API_KEY')
     );
   }
 }
