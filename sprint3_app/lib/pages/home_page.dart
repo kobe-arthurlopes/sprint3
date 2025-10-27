c5719789d8d2eb9bc4503d534ea70f2c379949ef
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-27 13:59:36 -0300

    feat: call articles list from home page

--- sprint3_app/lib/pages/home_page.dart
+++ sprint3_app/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sprint3_app/pages/details_page.dart';
 import 'package:sprint3_app/view_models/home_view_model.dart';
+import 'package:sprint3_app/widgets/all_articles_list.dart';
 import 'package:sprint3_app/widgets/news_sources_list.dart';
 
 class HomePage extends StatefulWidget {
@@ -24,27 +25,49 @@ class _HomePageState extends State<HomePage> {
   Future<void> _initialize() async {
     await _viewModel.start();
     await _viewModel.fetchNewsSources();
+    await _viewModel.fetchAllArticles();
   }
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<HomeData>(
-      valueListenable: _viewModel.homeData, 
+      valueListenable: _viewModel.homeData,
       builder: (_, data, _) {
         return Scaffold(
           appBar: AppBar(
-            title: const Text('News'),
+            // title: const Text('News'),
           ),
-          body: NewsSourcesList(
-            newsSources: data.newsSources,
-            onTap: (newsSource) {
-              _viewModel.updateSelectedNewsSource(newsSource);
-
-              Navigator.of(context).pushNamed(DetailsPage.routeId, arguments: _viewModel);
-            },
+          body: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  NewsSourcesList(
+                    newsSources: data.newsSources,
+                    onTap: (newsSource) {
+                      _viewModel.updateSelectedNewsSource(newsSource);
+            
+                      Navigator.of(context).pushNamed(DetailsPage.routeId, arguments: _viewModel);
+                    }
+                  ),
+            
+                  const SizedBox(height: 16),
+            
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: AllArticlesList(
+                      articles: data.articles,
+                      onTap: (article) {
+                        
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         );
-      }
+      } 
     );
   }
 }
