8177520669b5c31aeb7486a5619ca44bbbb669d2
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-28 14:48:21 -0300

    feat: add custom app bar widget to news source details page

--- sprint3_app/lib/pages/news_source_details_page.dart
+++ sprint3_app/lib/pages/news_source_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sprint3_app/models/article_model.dart';
 import 'package:sprint3_app/widgets/all_articles_list.dart';
+import 'package:sprint3_app/widgets/app_bar_widget.dart';
 
 class NewsSourceDetailsPage extends StatefulWidget {
   static const routeId = '/news_source_details';
@@ -22,9 +23,16 @@ class _NewsSourceDetailsPageState extends State<NewsSourceDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      backgroundColor: Color(0xFFF9FAFB),
+      appBar: AppBarWidget(
+        title: widget.title,
+      ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.only(
+          top: 10,
+          left: 10,
+          right: 10
+        ),
         child: AllArticlesList(
           articles: widget.articles,
         ),
