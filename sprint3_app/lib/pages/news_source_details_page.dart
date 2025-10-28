47e16a7b56ba32102dfb2db5b4b023e183ae4005
Arthur Lopes <arthurpsl99@gmail.com>
2025-10-28 00:35:12 -0300

    fix: news source details page receives title and articles directly

--- sprint3_app/lib/pages/news_source_details_page.dart
+++ sprint3_app/lib/pages/news_source_details_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:sprint3_app/view_models/home_view_model.dart';
+import 'package:sprint3_app/models/article_model.dart';
 import 'package:sprint3_app/widgets/all_articles_list.dart';
 
 class NewsSourceDetailsPage extends StatefulWidget {
   static const routeId = '/news_source_details';
 
-  final HomeViewModel viewModel;
+  final String title;
+  final List<ArticleModel> articles;
 
-  const NewsSourceDetailsPage({super.key, required this.viewModel});
+  const NewsSourceDetailsPage({
+    super.key,
+    required this.title,
+    required this.articles,
+  });
 
   @override
   State<StatefulWidget> createState() => _NewsSourceDetailsPageState();
@@ -16,39 +21,17 @@ class NewsSourceDetailsPage extends StatefulWidget {
 class _NewsSourceDetailsPageState extends State<NewsSourceDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<HomeData>(
-      valueListenable: widget.viewModel.homeData,
-      builder: (_, data, _) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(data.selectedNewsSource?.fields?.name ?? 'empty'),
-            leading: IconButton(
-              onPressed: () {
-                widget.viewModel.fetchAllArticles();
-
-                Navigator.of(context).pop();
-              },
-              icon: Icon(
-                Icons.arrow_back,
-                color: Colors.black,
-                size: 24
-              ),
-            ),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.only(
-              left: 10,
-              right: 10
-            ),
-            child: AllArticlesList(
-              articles: data.articles,
-              onTap: (article) {
-                print(article.title);
-              },
-            ),
-          ),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: AllArticlesList(
+          articles: widget.articles,
+          onTap: (article) {
+            print(article.title);
+          },
+        ),
+      ),
     );
   }
 }
