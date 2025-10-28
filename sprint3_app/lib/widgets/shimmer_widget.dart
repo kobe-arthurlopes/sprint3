da4378bf5920e3304c46828a8aab46d86ce29f2d
Arthur Lopes <arthur.lopes@kobe.io>
2025-10-28 14:46:30 -0300

    feat: craete custom shimmer widget

---
+++ sprint3_app/lib/widgets/shimmer_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerWidget extends StatelessWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
+  final Color baseColor;
+  final Color highlightColor;
+
+  const ShimmerWidget.rectangular({
+    super.key,
+    this.width = double.infinity,
+    required this.height,
+    required this.borderRadius,
+    required this.baseColor,
+    required this.highlightColor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: baseColor,
+      highlightColor: highlightColor,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+    );
+  }
+}
