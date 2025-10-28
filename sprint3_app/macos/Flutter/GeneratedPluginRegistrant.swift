fd3436f0a009ae08fb2166d293c3e520a6caa4b3
Arthur Lopes <arthurpsl99@gmail.com>
2025-10-28 01:14:32 -0300

    feat: update ios and android permissions in order for web view to work

--- sprint3_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sprint3_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_core
 import firebase_remote_config
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
