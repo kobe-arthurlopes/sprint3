75f97996db1af5880f421af145cd9b236f024668
Arthur Lopes <arthurpsl99@gmail.com>
2025-10-29 00:22:48 -0300

    fix: add graphql_flutter lib and remove contentful lib

--- sprint3_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sprint3_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import firebase_core
 import firebase_remote_config
+import path_provider_foundation
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
