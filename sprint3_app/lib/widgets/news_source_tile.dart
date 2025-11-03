9e58b0d748c286c22b2f992c43ca1ef88acfebaf
Arthur Lopes <arthur.lopes@kobe.io>
2025-11-03 14:52:04 -0300

    feat: remove unnecessary imports

--- sprint3_app/lib/widgets/news_source_tile.dart
+++ sprint3_app/lib/widgets/news_source_tile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sprint3_app/models/dto/news_source_dto_model.dart';
 import 'package:sprint3_app/theme/colors.dart';
-import 'package:sprint3_app/theme/image_paths.dart';
 import 'package:sprint3_app/widgets/model_image_widget.dart';
 import 'package:sprint3_app/widgets/shimmer_widget.dart';
 
@@ -32,26 +31,6 @@ class NewsSourceTile extends StatelessWidget {
                   imageUrl: newsSource!.logoUrl,
                   size: Size(100, 100)
                 )
-                
-                
-                
-                
-                
-                
-                // Image.network(
-                //   newsSource!.logoUrl ?? '',
-                //   width: 100,
-                //   height: 100,
-                //   fit: BoxFit.cover,
-                //   errorBuilder: (_, _, _) {
-                //     return Image.asset(
-                //       AppImagePaths.placeholder,
-                //       width: 100,
-                //       height: 100,
-                //       fit: BoxFit.cover,
-                //     );
-                //   },
-                // ),
               ),
 
             SizedBox(
