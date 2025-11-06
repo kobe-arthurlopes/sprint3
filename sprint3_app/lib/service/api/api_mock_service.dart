f2a4da3af29a53fb2f5af5855d32c1d76ebe0635
Arthur Lopes <arthur.lopes@kobe.io>
2025-11-06 15:01:04 -0300

    fix: add Dio logics do ApiMockService to check for internet connection

--- sprint3_app/lib/service/api/api_mock_service.dart
+++ sprint3_app/lib/service/api/api_mock_service.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:sprint3_app/service/api/api_exception.dart';
 import 'package:sprint3_app/service/api/api_service_protocol.dart';
 
 class ApiMockService implements ApiServiceProtocol {
+  final dio = Dio(BaseOptions(baseUrl: 'https://api.genderize.io?name=luc'));
+
   @override
   Future<T> fetchResponse<T>({
     String endpoint = 'top-headlines', 
@@ -32,9 +35,16 @@ class ApiMockService implements ApiServiceProtocol {
     final fileName = '${firstName}_response.json';
 
     try {
+      final _ = await dio.get('', queryParameters: {'name': 'luc'});
+
       final jsonString = await rootBundle.loadString('lib/json/$fileName');
       final data = json.decode(jsonString);
       return fromJson(data);
+    } on DioException catch (dioError) {
+      throw ApiException(
+        userMessage: 'Unable to connect. Please check your internet connection and try again. Status code: ${dioError.response?.statusCode}', 
+        debugMessage: 'Connection error: ${dioError.message}; No status code found.'
+      );
     } catch (error) {
       throw ApiException(
         userMessage: 'Error parsing json', 
