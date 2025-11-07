b82832904ce8ec7704919465268ce7f939c81f3c
Arthur Lopes <arthurpsl99@gmail.com>
2025-11-07 03:00:33 -0300

    chore: remove debug flag

--- sprint3_app/lib/main.dart
+++ sprint3_app/lib/main.dart
@@ -106,7 +106,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Nextra',
       onGenerateRoute: (settings) => _onGenerateRoute(context, settings),
       home: const HomePage(),
     );
